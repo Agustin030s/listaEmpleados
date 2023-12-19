4f4a31f9c9c369b45a5f193db0b2ce206e944907
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-19 20:07:48 -0300

    se agrego el maquetado de la lista, faltaria separar en componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Container } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import EmpleadoList from "./Components/EmpleadoList";
+import { Container } from "react-bootstrap";
 
 function App() {
   return(
-    <Container>
+    <Container className="my-5">
       <EmpleadoList></EmpleadoList>
     </Container>
   );
--- src/Components/EmpleadoList.jsx
+++ src/Components/EmpleadoList.jsx
@@ -1,9 +1,36 @@
+import { ListGroup, Image } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Badge from "react-bootstrap/Badge";
+
 const EmpleadoList = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+  return (
+    <ListGroup>
+      <ListGroup.Item>
+        <Row className="justify-content-center align-items-center">
+          <Col lg="2" className="text-center">
+            <Image
+              src="https://png.pngtree.com/png-vector/20190710/ourlarge/pngtree-user-vector-avatar-png-image_1541962.jpg"
+              fluid
+              roundedCircle
+              className="avatar"
+            ></Image>
+          </Col>
+          <Col lg="10">
+            <h3 className="text-center text-md-start">Usuario 1</h3>
+            <div className="d-flex justify-content-center justify-content-md-start align-items-center">
+              <p className="lead mb-2 me-2">CEO</p>
+              <Badge bg="info" className="mb-2" pill>
+                Bussines
+              </Badge>
+            </div>
+          </Col>
+        </Row>
+      </ListGroup.Item>
+      <ListGroup.Item>Usuario 2</ListGroup.Item>
+      <ListGroup.Item>Usuario 3</ListGroup.Item>
+    </ListGroup>
+  );
 };
 
 export default EmpleadoList;
