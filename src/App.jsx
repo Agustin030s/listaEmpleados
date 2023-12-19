002ffc3c196523f8634cab56065103322ed5dff2
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-19 19:11:19 -0300

    agregamos react boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import EmpleadoList from "./Components/EmpleadoList";
 
 function App() {
-  return <></>;
+  return(
+    <Container>
+      <EmpleadoList></EmpleadoList>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/Components/EmpleadoList.jsx
@@ -0,0 +1,9 @@
+const EmpleadoList = () => {
+    return (
+        <section>
+            
+        </section>
+    );
+};
+
+export default EmpleadoList;
